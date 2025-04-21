8a4ce85b3bd15b4eda3bf0af5979dcb8d1322965
datagore <104622881+datagore@users.noreply.github.com>
2025-04-21 21:53:40 +0300

    Write ft_strlen in terms of ft_strchr

--- ft_strlen.c
+++ ft_strlen.c
@@ -14,10 +14,5 @@
 
 size_t	ft_strlen(const char *string)
 {
-	size_t	length;
-
-	length = 0;
-	while (string[length] != '\0')
-		length++;
-	return (length);
+	return (ft_strchr(string, '\0') - string);
 }
